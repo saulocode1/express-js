e675f94c25e7be6248ccac4a60338b3ba4a7727b
Saulo <sauloveigr@hotmail.com>
2022-03-14 18:08:53 -0300

    Create about path

--- basic/04 - express/app.js
+++ basic/04 - express/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => { // get path "/"
-    res.send("Teste"); // send "Teste" to path "/"
+    res.send("Welcome!"); // send "Teste" to path "/"
 });
+
+app.get("/about", (req, res) => {
+    res.send("About.")
+})
 
 app.listen(8081, () => { // open server on 8081 port
     console.log("Server online!");
